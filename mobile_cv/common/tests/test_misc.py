7b727fdce74e87047c0201498c70259c06b84404
stzpz <stzpz@fb.com>
2021-02-17 15:10:18 -0800

    Update common

--- mobile_cv/common/tests/test_misc.py
+++ mobile_cv/common/tests/test_misc.py
@@ -5,7 +5,7 @@ import os
 import unittest
 
 from mobile_cv.common.misc.file_utils import make_temp_directory
-from mobile_cv.common.misc.imports import import_file
+from mobile_cv.common.misc.py import dynamic_import, import_file
 
 
 class TestMisc(unittest.TestCase):
@@ -17,3 +17,7 @@ class TestMisc(unittest.TestCase):
 
             my_module = import_file("my_module", filename)
             self.assertEqual(my_module.ANSWER, 42)
+
+    def test_dynamic_import(self):
+        func = dynamic_import("mobile_cv.common.misc.py.dynamic_import")
+        self.assertEqual(func, dynamic_import)
