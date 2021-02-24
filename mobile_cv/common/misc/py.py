0c39be66d36d2c651daf617fca378e829bce656b
Hang Zhang <zhanghang@fb.com>
2021-02-24 09:25:39 -0800

    address comments

--- mobile_cv/common/misc/py.py
+++ mobile_cv/common/misc/py.py
@@ -16,26 +16,16 @@ import traceback
 logger = logging.getLogger(__name__)
 
 
-if sys.version_info[0] == 3:
+def import_file(module_name, file_path, make_importable=False):
     import importlib
     import importlib.util
 
-    # from https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path  # NOQA
-    def import_file(module_name, file_path, make_importable=False):
-        spec = importlib.util.spec_from_file_location(module_name, file_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        if make_importable:
-            sys.modules[module_name] = module
-        return module
-
-
-else:
-    import imp
-
-    def import_file(module_name, file_path, make_importable=None):
-        module = imp.load_source(module_name, file_path)
-        return module
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    if make_importable:
+        sys.modules[module_name] = module
+    return module
 
 
 def dynamic_import(obj_full_name):
