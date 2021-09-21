04a8a5be2e4abd6a625d75f0840238c9a33905c2
Hang Zhang <zhanghang@fb.com>
2021-09-21 16:51:55 -0700

    fix import

--- mobile_cv/model_zoo/datasets/__init__.py
+++ mobile_cv/model_zoo/datasets/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 
 # to register datasets
-from . import dataset_classy  # noqa
 from . import dataset_simple  # noqa
--- mobile_cv/model_zoo/tasks/__init__.py
+++ mobile_cv/model_zoo/tasks/__init__.py
@@ -1,4 +1,3 @@
 #!/usr/bin/env python3
 
-from . import task_classy  # noqa
 from . import task_general  # noqa
