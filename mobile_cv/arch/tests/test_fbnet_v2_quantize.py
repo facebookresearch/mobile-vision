264418a2f852e595e3dfbddf7de77d818de984b9
Peizhao Zhang <stzpz@fb.com>
2022-02-06 17:29:17 -0800

    Try static qat for lstm.
    Summary:
    Try static qat for lstm.
    * Verify if we could run lstm with QAT.
    
    Reviewed By: zhanghang1989
    
    Differential Revision: D33848135
    
    fbshipit-source-id: 77bceb63f5624fc5cdf326aa208116e1c6b918e4

--- mobile_cv/arch/tests/test_fbnet_v2_quantize.py
+++ mobile_cv/arch/tests/test_fbnet_v2_quantize.py
@@ -7,6 +7,7 @@ import mobile_cv.arch.fbnet_v2.fbnet_builder as fbnet_builder
 import mobile_cv.arch.utils.fuse_utils as fuse_utils
 import mobile_cv.arch.utils.quantize_utils as qu  # noqa
 import torch
+import torch.ao.quantization.quantize_fx as quantize_fx
 
 
 def _build_model(arch_def, dim_in):
@@ -63,3 +64,86 @@ class TestFBNetV2Quantize(unittest.TestCase):
         jit_model = torch.jit.trace(quant_model, data)
         jit_quant_output = jit_model(torch.rand([2, 3, 8, 8]))
         self.assertEqual(jit_quant_output.shape, torch.Size([2, 8, 2, 2]))
+
+    def test_qat(self):
+        e6 = {"expansion": 6}
+        dw_skip_bnrelu = {"dw_skip_bnrelu": True}
+        bn_args = {"bn_args": {"name": "bn", "momentum": 0.003}}
+        arch_def = {
+            "blocks": [
+                # [op, c, s, n, ...]
+                # stage 0
+                [("conv_k3", 4, 2, 1, bn_args)],
+                # stage 1
+                [
+                    ("ir_k3_sehsig", 8, 2, 2, e6, dw_skip_bnrelu, bn_args),
+                    ("ir_k5", 8, 1, 1, e6, bn_args),
+                ],
+            ]
+        }
+
+        model = _build_model(arch_def, dim_in=3)
+        model.train()
+
+        qconfig_dict = {"": torch.ao.quantization.get_default_qat_qconfig("fbgemm")}
+        model_prepared = quantize_fx.prepare_qat_fx(model, qconfig_dict)
+        print(f"Prepared model {model_prepared}")
+
+        # calibration
+        for _ in range(5):
+            data = torch.rand([2, 3, 8, 8])
+            model(data)
+
+        model_prepared.eval()
+        model_quant = quantize_fx.convert_fx(model_prepared)
+
+        print(f"Quantized model {model_quant}")
+
+        # # Run quantized model
+        quant_output = model_quant(torch.rand([2, 3, 8, 8]))
+        self.assertEqual(quant_output.shape, torch.Size([2, 8, 2, 2]))
+
+        # Trace quantized model
+        jit_model = torch.jit.trace(model_quant, data)
+        jit_quant_output = jit_model(torch.rand([2, 3, 8, 8]))
+        self.assertEqual(jit_quant_output.shape, torch.Size([2, 8, 2, 2]))
+
+    def test_qat_lstm(self):
+        import torch.nn.quantizable as nnqa
+
+        model = torch.nn.Sequential(
+            torch.quantization.QuantStub(),
+            torch.nn.LSTM(input_size=10, hidden_size=20, num_layers=2),
+            # torch.quantization.DeQuantStub()
+        )
+
+        from torch.ao.quantization.quantization_mappings import (
+            get_default_qat_module_mappings,
+        )
+
+        model.train()
+        model.qconfig = torch.ao.quantization.get_default_qat_qconfig("fbgemm")
+        model_prepared = torch.ao.quantization.prepare_qat(
+            model,
+            mapping={torch.nn.LSTM: nnqa.LSTM, **get_default_qat_module_mappings()},
+        )
+        print(f"Prepared model {model_prepared}")
+
+        # calibration
+        for _ in range(5):
+            data = torch.rand([4, 2, 10])
+            model(data)
+
+        model_prepared.eval()
+        model_quant = torch.ao.quantization.convert(model_prepared)
+
+        print(f"Quantized model {model_quant}")
+
+        # # Run quantized model
+        quant_output, (hn, cn) = model_quant(torch.rand([4, 2, 10]))
+        self.assertEqual(quant_output.shape, torch.Size([4, 2, 20]))
+
+        # Trace quantized model
+        jit_model = torch.jit.trace(model_quant, data)
+        jit_quant_output, jit_hcn = jit_model(torch.rand([4, 2, 10]))
+        self.assertEqual(jit_quant_output.shape, torch.Size([4, 2, 20]))
