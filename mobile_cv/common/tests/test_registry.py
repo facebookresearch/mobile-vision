48a85a11f158a0e93b81dc42e732ddb2da94f944
Yanghan Wang <yanghan@fb.com>
2022-05-25 11:55:07 -0700

    support lazy object in registry
    Summary: Original `Registry` support register Function/Class. This diff supports register lazy version of Function/Class, which is represented by a string containing "module-dot-name" (eg. a class with name `MyClass` under module `d2go/a/b/c.py` will be represented as `d2go.a.b.c.MyClass`).
    
    Reviewed By: tglik
    
    Differential Revision: D36414579
    
    fbshipit-source-id: e8609d4b0057deeea5f62e8ed675c2d52e59ad65

--- mobile_cv/common/tests/test_registry.py
+++ mobile_cv/common/tests/test_registry.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import os
+import sys
 import unittest
+import uuid
 
+from mobile_cv.common.misc.file_utils import make_temp_directory
 from mobile_cv.common.misc.registry import Registry
 
 
 def test_func():
     return "test_func"
+
+
+TEST_LAZY_REGISTRY = Registry("test_lazy")
 
 
 class TestRegistry(unittest.TestCase):
@@ -25,3 +32,28 @@ class TestRegistry(unittest.TestCase):
         self.assertEqual(out, "test_func")
         out1 = REG_LIST.get("test_func1")()
         self.assertEqual(out1, "test_func_1")
+
+    def test_lazy_registration(self):
+        random_package_name = f"random_package_{str(uuid.uuid4().hex)[:8]}"
+        self.assertTrue(random_package_name not in sys.modules)
+
+        # we can even do lazy register before the code exists
+        TEST_LAZY_REGISTRY.register("lazy_func1", f"{random_package_name}.lazy_func1")
+
+        with make_temp_directory("test_lazy_registration") as root:
+            os.makedirs(os.path.join(root, random_package_name))
+            with open(os.path.join(root, random_package_name, "__init__.py"), "w") as f:
+                f.write(
+                    f"""
+from {__package__}.test_registry import TEST_LAZY_REGISTRY
+@TEST_LAZY_REGISTRY.register()
+def lazy_func1():
+    return "lazy_func1"
+"""
+                )
+            # make test files import-able
+            sys.path.append(root)
+
+            # resolve the lazy registered object
+            lazy_func1 = TEST_LAZY_REGISTRY.get("lazy_func1")
+            self.assertEqual(lazy_func1(), "lazy_func1")
