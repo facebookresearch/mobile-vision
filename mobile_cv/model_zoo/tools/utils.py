a5078a39f95b44465d0e7a50842495d06313aee5
Mircea Cimpoi <mcimpoi@fb.com>
2022-06-28 13:59:36 -0700

    Use traceable model and flattened inputs if available
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/87
    
    Moved `get_model_attributes` to util, to reuse it in other places in the code.
    
    Reviewed By: wat3rBro
    
    Differential Revision: D36999453
    
    fbshipit-source-id: 1f0b16391f56ea5b8e38111fcfa997c3b14b1508

--- mobile_cv/model_zoo/tools/utils.py
+++ mobile_cv/model_zoo/tools/utils.py
@@ -4,6 +4,8 @@ import json
 import os
 import shutil
 
+import torch
+
 
 def copy_file(src, dst, skip_exists=True):
     if os.path.exists(src):
@@ -17,3 +19,14 @@ def copy_file(src, dst, skip_exists=True):
 def save_json(file, data):
     with open(file, "w") as outfile:
         json.dump(data, outfile, indent=4, sort_keys=True)
+
+
+def get_model_attributes(model: torch.nn.Module):
+    model_attrs = None
+    if hasattr(model, "attrs"):
+        model_attrs = model.attrs
+        if model_attrs is not None:
+            assert isinstance(
+                model_attrs, dict
+            ), f"Invalid model attributes type: {model_attrs}"
+    return model_attrs
