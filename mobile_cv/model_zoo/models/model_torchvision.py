d3fc30f83de523f0d6e736a3a3788eec31292871
Vasilis Vryniotis <vvryniotis@fb.com>
2022-09-12 10:37:12 -0700

    Cross-version compatibility support for TorchVision's model builders
    Summary: Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/105
    
    Reviewed By: jdsgomes
    
    Differential Revision: D39431114
    
    fbshipit-source-id: 6442cd173b4e22cc6fcde5fdee457360bbd932cc

--- mobile_cv/model_zoo/models/model_torchvision.py
+++ mobile_cv/model_zoo/models/model_torchvision.py
@@ -7,24 +7,49 @@ Models from torchvision, providing the same interface for loading in model zoo
 
 import torchvision
 from mobile_cv.model_zoo.models.hub_utils import pretrained_download
+from pkg_resources import parse_version
 
 # to register for model_zoo
 from . import model_zoo_factory  # noqa
 
 
-if hasattr(torchvision.models, "list_models"):
-    # TorchVision >= v0.14
-    tv_models = torchvision.models.list_models(torchvision.models)
+_tv_version = parse_version(torchvision.__version__)
+
+
+if _tv_version >= parse_version("0.14.0a0"):
+    _tv_models = torchvision.models.list_models(torchvision.models)
+    _get_model_builder = torchvision.models.get_model_builder
 else:
-    tv_models = [
+    _tv_models = [
         v.__name__
         for k, v in torchvision.models.__dict__.items()
         if callable(v) and k[0].islower() and k[0] != "_" and k != "get_weight"
     ]
 
-for model_name in tv_models:
+    def _get_model_builder(model_name):
+        return torchvision.models.__dict__[model_name]
+
+
+def _get_builder(model_name):
+    def wrapped(**kwargs):
+        model_fn = _get_model_builder(model_name)
+        if kwargs.pop("pretrained", False):
+            if _tv_version >= parse_version("0.14.0a0"):
+                kwargs["weights"] = torchvision.models.get_model_weights(
+                    model_fn
+                ).from_str("IMAGENET1K_V1")
+            elif _tv_version >= parse_version("0.13"):
+                kwargs["weights"] = "IMAGENET1K_V1"
+            else:
+                kwargs["pretrained"] = True
+        return model_fn(**kwargs)
+
+    return wrapped
+
+
+for model_name in _tv_models:
     if model_name not in model_zoo_factory.MODEL_ZOO_FACTORY:
         model_zoo_factory.MODEL_ZOO_FACTORY.register(
             model_name,
-            pretrained_download(torchvision.models.__dict__[model_name]),
+            pretrained_download(_get_builder(model_name)),
         )
