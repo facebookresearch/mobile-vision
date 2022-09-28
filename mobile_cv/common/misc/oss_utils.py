e557c4ba034af9430fa247d95bc5a15f7af6c3ec
Zhanibek Datbayev <datbayev@fb.com>
2022-09-27 18:15:03 -0700

    add assert for fb_overwritable decorator.
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/108
    
    X-link: https://github.com/facebookresearch/d2go/pull/373
    
    Asserting that methods replaced by fb_overwritable are also annotated with corresponding decorator.
    
    Reviewed By: wat3rBro
    
    Differential Revision: D39674347
    
    fbshipit-source-id: 6915127a7cb501a4c2bd08363968e879586c52c2

--- mobile_cv/common/misc/oss_utils.py
+++ mobile_cv/common/misc/oss_utils.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import inspect
+
+from typing import Any
+
 from mobile_cv.common.misc.py import dynamic_import
+
+fb_overwritable_funcs = set()
 
 
 def is_oss():
@@ -16,16 +22,48 @@ def is_oss():
     return ret
 
 
+def check_for_decorator(func: Any):
+    """
+    Returns True if given :func (method or class) has fb_overwritable decorator.
+    Return False otherwise.
+    """
+    if not inspect.isfunction(func) and not inspect.isclass(func):
+        return False
+
+    global fb_overwritable_funcs
+    module_and_name = func.__module__ + "." + func.__name__
+    return fb_overwritable_funcs and (module_and_name in fb_overwritable_funcs)
+
+
 def fb_overwritable():
     """Decorator on function that has alternative internal implementation"""
 
-    def deco(oss_func):
-        if is_oss():
-            return oss_func
-        else:
-            oss_module = oss_func.__module__
-            fb_module = oss_module + "_fb"  # xxx.py -> xxx_fb.py
-            fb_func = dynamic_import("{}.{}".format(fb_module, oss_func.__name__))
-            return fb_func
+    def _enforce_decorator(func):
+        if not check_for_decorator(func):
+            # NOTE: we should create a set of custom exceptions for d2go for easier
+            # tracking of various errors.
+            raise Exception(
+                "Missing @fb_overwritable decorator for {}.{}.".format(
+                    func.__module__, func.__name__
+                )
+            )
+
+    def deco(func):
+        global fb_overwritable_funcs
+
+        module_name = func.__module__
+        fb_overwritable_funcs.add(module_name + "." + func.__name__)
+
+        # If this is a method that's from "_fb" module then we simply return method
+        # as is, no replacement is necessary.
+        if module_name.endswith("_fb") or is_oss():
+            return func
+
+        fb_module = module_name + "_fb"  # xxx.py -> xxx_fb.py
+        fb_func = dynamic_import("{}.{}".format(fb_module, func.__name__))
+
+        _enforce_decorator(fb_func)
+
+        return fb_func
 
     return deco
