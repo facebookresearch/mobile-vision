57c7c5bce0de602f966ffb56850116476d34a266
Yanghan Wang <yanghan@meta.com>
2022-10-12 12:18:00 -0700

    initial setup
    Summary: Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/112
    
    Reviewed By: tglik
    
    Differential Revision: D40134340
    
    fbshipit-source-id: 72e935b13e97d0f1f482482e57c24f5776261c93

---
+++ mobile_cv/exporter_v2/api.py
@@ -0,0 +1,28 @@
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+
+from typing import List
+
+import torch.nn as nn
+from mobile_cv.exporter_v2.converter import ConverterBase
+
+
+def export_models(
+    converter: ConverterBase, output_dir: str, export_targets: List[str]
+) -> List[str]:
+    """
+    # Vanilla implementation:
+    paths = []
+    for export_target in export_targets:
+        convert_target, backend, additional_flags = _parse(export_target)
+        model = _convert(converter, convert_target)
+        path = _save(model, backend, additional_flags)
+        paths.append(path)
+    return paths
+    """
+
+    raise NotImplementedError()
+
+
+def load_exported_model(path: str) -> nn.Module:
+    raise NotImplementedError()
