5eeac6d8a1c5d7b26240f2821b5055565be6c100
Naveen Suda <naveensuda@meta.com>
2023-05-25 09:55:43 -0700

    utility to compress exported models
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/153
    
    Add utility to compress exported torchscript models
    
    Reviewed By: svenkreiss
    
    Differential Revision: D46011302
    
    fbshipit-source-id: add8751a0f228893f9f30a1e3e4b557a88eccab9

--- mobile_cv/common/misc/file_utils.py
+++ mobile_cv/common/misc/file_utils.py
@@ -4,6 +4,9 @@
 import contextlib
 import shutil
 import tempfile
+import zipfile
+
+from iopath.common.file_io import g_pathmgr
 
 
 @contextlib.contextmanager
@@ -13,3 +16,25 @@ def make_temp_directory(prefix):
         yield temp_dir
     finally:
         shutil.rmtree(temp_dir)
+
+
+def local_recompress_export(inputfile: str, outputfile: str) -> None:
+    with zipfile.ZipFile(inputfile, "r") as zip_input:
+        with zipfile.ZipFile(
+            outputfile,
+            "w",
+            compression=zipfile.ZIP_DEFLATED,
+        ) as zip_output:
+            for filename in zip_input.namelist():
+                with zip_input.open(filename, "r") as f_input:
+                    data = f_input.read()
+                with zip_output.open(filename, "w") as f_output:
+                    f_output.write(data)
+
+
+def recompress_export(inputfile: str, outputfile: str) -> None:
+    local_inputfile = g_pathmgr.get_local_path(inputfile, force=True)
+    with tempfile.TemporaryDirectory() as tempdir:
+        local_outputfile = f"{tempdir}/out.zip"
+        local_recompress_export(local_inputfile, local_outputfile)
+        g_pathmgr.copy_from_local(local_outputfile, outputfile)
