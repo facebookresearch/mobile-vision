4aab7cc8274071e7f81ea56636a3a6a75ae4a5ec
Yanghan Wang <yanghan@meta.com>
2023-06-15 11:21:35 -0700

    remove `import *`
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/163
    
    update import path, remove files:
    - mobile-vision/mobile_cv/mobile_cv/arch/fbnet_v2/multi_io.py
    - mobile-vision/mobile_cv/mobile_cv/arch/fbnet_v2/fb/fbnet_hr.py
    
    Reviewed By: jiaxuzhu92
    
    Differential Revision: D46688571
    
    fbshipit-source-id: dbbfcdb437539cf06fd764baf1375253451e2ab5

--- mobile_cv/arch/fbnet_v2/fbnet_hr_modeldef.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-"""
-This file is for backward compatiblity.
-Use mobile_cv.arch.builder.fb.fbnet_hr_modeldef instead.
-"""
-
-# @fb-only: from mobile_cv.arch.builder.fb.fbnet_hr_modeldef import *  # noqa  
