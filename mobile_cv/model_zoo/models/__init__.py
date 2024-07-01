e2e5614f763c0325e8dddab14601fda79dc40a7b
Yanghan Wang <yanghan@meta.com>
2024-07-01 12:08:31 -0700

    clean up mobile-vision/mobile_cv/mobile_cv/model_zoo/models
    Summary: See summary of D59237876
    
    Differential Revision: D59238233

--- mobile_cv/model_zoo/models/__init__.py
+++ mobile_cv/model_zoo/models/__init__.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+
+from mobile_cv.common.misc.oss_utils import fb_overwritable
+
+
 # to register for model_zoo
-from mobile_cv.model_zoo.models import fbnet_v2, model_jit, model_torchvision  # noqa
+@fb_overwritable()
+def register_models():
+    from mobile_cv.model_zoo.models import (  # noqa
+        fbnet_v2,  # noqa
+        model_jit,  # noqa
+        model_torchvision,  # noqa
+    )
+
+
+register_models()
