ee3a3c06ac9ab2034cc1d49e81d5f26d6dde6535
Yanghan Wang <yanghan@meta.com>
2024-07-08 08:54:25 -0700

    clean up mobile-vision/mobile_cv/mobile_cv/model_zoo/tasks
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/mobile-vision/pull/198
    
    See summary of D59237876
    
    Reviewed By: rbasch
    
    Differential Revision: D59239507
    
    fbshipit-source-id: f8f57167a9642c672d87273f0d5d632cb8435cc2

--- mobile_cv/model_zoo/tasks/__init__.py
+++ mobile_cv/model_zoo/tasks/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from . import task_general  # noqa
+from mobile_cv.common.misc.py import import_matching_modules
+
+
+import_matching_modules("mobile_cv.model_zoo.tasks", "task_general")
+# @fb-only: import_matching_modules("mobile_cv.model_zoo.tasks.fb", "*") 
--- mobile_cv/model_zoo/tasks/task_common.py
+++ mobile_cv/model_zoo/tasks/task_common.py
@@ -5,9 +5,11 @@
 Task with some common task implementation
 """
 
+from mobile_cv.common.misc.oss_utils import fb_overwritable
 from mobile_cv.model_zoo.tasks.task_base import TaskBase
 
 
 # task with common implementations
+@fb_overwritable()
 class TaskCommon(TaskBase):
     pass
